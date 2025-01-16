906005b65c812f69a2f7b7c5eadeb0eb91ed1d03
barbotine <barbot.florieanne@gmail.com>
2025-01-16 10:56:48 +0100

    feat : add boat sprite on screen

--- BoatJourney/Main.cpp
+++ BoatJourney/Main.cpp
@@ -1,21 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+using namespace std;
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
+    unsigned int screenWidth = desktopMode.size.x;
+    unsigned int screenHeight = desktopMode.size.y;
 
-    while (window.isOpen())
+    sf::RenderWindow window(desktopMode, "Plein écran");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("C:/Main/BoatJourney/assets/texture/boat.png")){
+        return -1;
+    }
+ 
+    sf::Sprite boat(texture);
+
+    float positionBoatX = 400.f;
+    float positionBoatY = window.getSize().y * 0.5f;
+
+    sf::Vector2f position(positionBoatX, positionBoatY);
+    boat.setPosition(position);
+
+    while(window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-
         window.clear();
-        window.draw(shape);
+        window.draw(boat);
         window.display();
     }
+    return 0; 
 }
