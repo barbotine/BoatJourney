b0c429994ba74a2af54d030221ec965ed002b95c
barbotine <barbot.florieanne@gmail.com>
2025-01-16 21:14:46 +0100

    feat : create water and make background responsive

--- BoatJourney/Main.cpp
+++ BoatJourney/Main.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 int main()
 {
     sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
@@ -13,18 +12,52 @@ int main()
 
     sf::RenderWindow window(desktopMode, "Plein écran");
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("C:/Main/BoatJourney/assets/texture/boat.png")){
+    sf::RectangleShape background(sf::Vector2f(window.getSize()));
+    sf::Vector2f backgroundPosition(0.f, 0.f);
+    background.setPosition(backgroundPosition);
+
+    sf::Texture boatTexture, bg, cloudTexture;
+    if (!boatTexture.loadFromFile("../assets/texture/boat.png") || 
+        !bg.loadFromFile("../assets/texture/bg.jpg") ||
+        !cloudTexture.loadFromFile("../assets/texture/cloud.png")
+        ){
         return -1;
     }
  
-    sf::Sprite boat(texture);
+    // Charger le shader
+    sf::Shader shader;
+
+    if (!shader.loadFromFile("../assets/shaders/water_shader.frag", sf::Shader::Type::Fragment)) {
+        std::cerr << "Erreur lors du chargement du shader !" << std::endl;
+        return -1;
+    }
+    sf::Sprite boat(boatTexture);
+    sf::Sprite cloud(cloudTexture);
 
     float positionBoatX = 400.f;
-    float positionBoatY = window.getSize().y * 0.5f;
+    float positionBoatY = window.getSize().y * 0.7f;
 
     sf::Vector2f position(positionBoatX, positionBoatY);
     boat.setPosition(position);
+
+    float cloudX = 400.f;
+    float cloudY = window.getSize().y * 0.1f;
+
+    sf::Vector2f cloudPosition(cloudX, cloudY);
+    boat.setPosition(cloudPosition);
+
+    sf::Color color = sf::Color::Blue;
+
+    sf::Clock clock;
+
+    sf::Angle rotationAngle = sf::degrees(2);
+
+    float boatWidth = boat.getLocalBounds().size.x / 2;
+    float boatHeight = boat.getLocalBounds().size.y / 2;
+
+    sf::Vector2f boatOrigin(boatWidth, boatHeight);
+
+    boat.setOrigin(boat.getLocalBounds().size / 2.0f);
 
     while(window.isOpen())
     {
@@ -33,8 +66,19 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+
+        float time = clock.getElapsedTime().asSeconds();
+      
+
+        // Passer le temps au shader
+        shader.setUniform("time", time);
+        shader.setUniform("resolution", sf::Vector2f(window.getSize()));
+        shader.setUniform("backgroundTex", bg);
+
         window.clear();
+        window.draw(background, &shader);
         window.draw(boat);
+        window.draw(cloud);
         window.display();
     }
     return 0; 
---
+++ assets/shaders/water_shader.frag
@@ -0,0 +1,37 @@
+#version 330 core
+
+uniform float time;
+uniform vec2 resolution;   
+uniform sampler2D backgroundTex;
+uniform float textureScale;
+
+float wave(vec2 uv, float frequency, float amplitude, float speed, float phase) {
+    return sin(uv.x * frequency + time * speed + phase) * amplitude;
+}
+
+void main() {
+    vec2 uv = gl_FragCoord.xy / resolution.xy;
+
+    uv.y = 1.0 - uv.y;
+
+    uv.x *= resolution.x / resolution.y;
+
+    float y = 0.0;
+    y += wave(uv, 10.0, 0.03, 2.0, 0.0); 
+    y += wave(uv, 15.0, 0.02, 3.0, 1.0);
+    y += wave(uv, 20.0, 0.01, 1.5, 2.0);
+
+    vec3 color = vec3(0.0, 0.3, 0.6);
+
+    if (uv.y < 0.5 + y) {
+        vec2 texCoord = uv * textureScale;
+        color = texture(backgroundTex, texCoord).rgb;
+    }
+
+    if (uv.y > 0.5 + y) {
+        vec3 waveColor = vec3(0.25, 0.75, 0.75) * 0.6;
+        color = mix(color, waveColor, 0.6);
+    }
+
+    gl_FragColor = vec4(color, 1.0);
+}
